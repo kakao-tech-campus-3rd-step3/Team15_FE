9b9d010c1e986e54dc887350bee621d9bd1e584a
youngseo <givpro22@daum.net>
2025-08-31 01:18:04 +0900

    chore: 테스트용 server 및 setupTests.ts 추가

---
+++ src/test/setupTests.ts
@@ -0,0 +1,7 @@
+/// <reference types="vitest/globals" />
+import "@testing-library/jest-dom";
+import { server } from "../mocks/server";
+
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
