5753c3e7d345e02af6b9b9c16ecde6b59d966284
youngseo <givpro22@daum.net>
2025-09-04 14:33:33 +0900

    refactor: 라우트 상수 수정

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -2,15 +2,16 @@ import { DevPanel } from '@/shared/ui';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppLayout from '../layout/AppLayout';
 import { LandingPage } from '@/pages/landing';
+import { ROUTES } from '@/shared/config';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AppLayout />}>
-          <Route path='/' element={<LandingPage />} />
-          {/* <Route path='/home' element={<HomePage />} /> */}
-          {/* <Route path="/profile" element={<Profile />} /> */}
+          <Route path={ROUTES.landing} element={<LandingPage />} />
+          {/* <Route path={ROUTES.home} element={<HomePage />} /> */}
+          {/* <Route path={ROUTES.login} element={<Login />} /> */}
         </Route>
       </Routes>
       <DevPanel />
