e805c2c74d51945a5bcb2824f5103164b8cf49b3
youngseo <givpro22@daum.net>
2025-09-09 02:40:06 +0900

    feat: sidebar에 상단에 '휴쉼' 작성

--- src/widgets/sidebar/ui/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Sidebar } from '@/shared/ui/shadcn/sidebar';
+import { Sidebar, SidebarHeader } from '@/shared/ui/shadcn/sidebar';
 
 import SidebarContentSection from './SidebarContentSection';
 import SidebarFooterSection from './SidebarFooterSection';
@@ -6,6 +6,9 @@ import SidebarFooterSection from './SidebarFooterSection';
 export function SideBar() {
   return (
     <Sidebar>
+      <SidebarHeader>
+        <span className='block w-full text-center text-xl font-bold'>휴 쉼</span>
+      </SidebarHeader>
       <SidebarContentSection />
       <SidebarFooterSection />
     </Sidebar>
