14357af768a2f8e5c1c2879043fc9050a773534e
youngseo <givpro22@daum.net>
2025-09-09 02:27:59 +0900

    feat: SideBar드롭다운에 설정 메뉴 추가

--- src/widgets/sidebar/ui/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar.tsx
@@ -1,45 +1,13 @@
-import {
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarHeader,
-  Sidebar,
-  SidebarMenuItem,
-} from '@/shared/ui/shadcn/sidebar';
-import { LayoutGrid, MessageCircle, NotebookText, FileText } from 'lucide-react';
+import { Sidebar } from '@/shared/ui/shadcn/sidebar';
+
+import SidebarContentSection from './SidebarContentSection';
+import SidebarFooterSection from './SidebarFooterSection';
 
 export function SideBar() {
   return (
     <Sidebar>
-      <SidebarHeader />
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarMenuItem>
-            <div className='flex items-center gap-2'>
-              <LayoutGrid />홈
-            </div>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <div className='flex items-center gap-2'>
-              <NotebookText />
-              미션
-            </div>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <div className='flex items-center gap-2'>
-              <MessageCircle />
-              마음소식
-            </div>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <div className='flex items-center gap-2'>
-              <FileText />
-              지원사업
-            </div>
-          </SidebarMenuItem>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter />
+      <SidebarContentSection />
+      <SidebarFooterSection />
     </Sidebar>
   );
 }
