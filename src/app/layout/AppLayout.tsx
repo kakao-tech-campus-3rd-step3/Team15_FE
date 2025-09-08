9523a2eb77987d48bfc7a9026f9699a68daf3413
youngseo <givpro22@daum.net>
2025-09-09 01:59:28 +0900

    refactor: 중복된 레이아웃 구조 제거 및 Sidebar 조건부 적용

--- src/app/layout/AppLayout.tsx
+++ src/app/layout/AppLayout.tsx
@@ -1,15 +1,32 @@
 import { Outlet } from 'react-router-dom';
-import { Header } from '@/widgets/header';
 import { Footer } from '@/widgets/footer';
 import { Separator } from '@/shared/ui/shadcn/separator';
+import { SidebarProvider, SidebarTrigger } from '@/shared/ui/shadcn/sidebar';
+import { SideBar } from '@/widgets/sidebar';
+import { HeaderGuest } from '@/widgets/header';
 
 export default function AppLayout() {
+  const isLoggedIn = false;
   return (
     <>
-      <Header />
-      <Separator />
-      <Outlet />
-      <Footer />
+      {isLoggedIn ? (
+        <>
+          <HeaderGuest />
+          <Separator />
+          <Outlet />
+          <Footer />
+        </>
+      ) : (
+        <SidebarProvider>
+          <SideBar />
+          <main>
+            <SidebarTrigger />
+            <Separator />
+            <Outlet />
+            <Footer />
+          </main>
+        </SidebarProvider>
+      )}
     </>
   );
 }
--- src/widgets/sidebar/ui/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar.tsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import {
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  Sidebar,
+  SidebarMenuItem,
+} from '@/shared/ui/shadcn/sidebar';
+import { LayoutGrid, MessageCircle, NotebookText, FileText } from 'lucide-react';
 
-function Sidebar() {
-  return <div>Sidebar</div>;
+export function SideBar() {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenuItem>
+            <div className='flex items-center gap-2'>
+              <LayoutGrid />홈
+            </div>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <div className='flex items-center gap-2'>
+              <NotebookText />
+              미션
+            </div>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <div className='flex items-center gap-2'>
+              <MessageCircle />
+              마음소식
+            </div>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <div className='flex items-center gap-2'>
+              <FileText />
+              지원사업
+            </div>
+          </SidebarMenuItem>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
 }
-
-export default Sidebar;
