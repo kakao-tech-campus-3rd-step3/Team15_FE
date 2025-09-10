f081d88aacc0fedd926b0a1455ec4d6232795cc0
jinhyun <jin71744@gmail.com>
2025-09-11 00:52:47 +0900

    style: login page 배경색 수정

--- src/pages/auth/ui/AuthPage.tsx
+++ src/pages/auth/ui/AuthPage.tsx
@@ -3,7 +3,7 @@ import { AuthLayout } from './AuthLayout';
 
 export function AuthPage() {
   return (
-    <div className='flex min-h-screen items-center justify-center bg-gradient-to-br from-green-400 to-blue-400 p-4 font-sans sm:p-6 md:p-0'>
+    <div className='flex min-h-screen items-center justify-center bg-gradient-to-br from-green-50 to-emerald-200 p-4 font-sans sm:p-6 md:p-0'>
       <AuthLayout>
         <AuthFormTabs />
       </AuthLayout>
