fecdfb398c7f2a201dc6d3a4f67bb53d500c8443
youngseo <givpro22@daum.net>
2025-09-19 22:50:58 +0900

    feat: 게시글 스위치 토글 ui 생성

--- src/pages/heartNews/ui/HeartNewsPage.tsx
+++ src/pages/heartNews/ui/HeartNewsPage.tsx
@@ -10,8 +10,21 @@ export function HeartNewsPage() {
   return (
     <>
       <PostsHeader />
-      <FilterTabs category={category} setCategory={setCategory} />
-      <ViewSwitch />
+
+      {/* 카테고리 탭 + 우측 툴바(뷰스위치) 한 줄 배치 */}
+      <div className='flex items-center px-5'>
+        {/* 좌측: 탭 (가로 스크롤 허용) */}
+        <div className='flex-1 overflow-x-auto'>
+          <div className='min-w-max'>
+            <FilterTabs category={category} setCategory={setCategory} />
+          </div>
+        </div>
+        {/* 우측: 툴바 */}
+        <div className='ml-3 flex items-center gap-3 border-l pl-3'>
+          <ViewSwitch />
+        </div>
+      </div>
+
       <PostList className='mt-8' code={category} />
     </>
   );
