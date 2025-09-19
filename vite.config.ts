0c076a7c17d660b849fbde4fb005abd074e2e0e4
jinhyun <jin71744@gmail.com>
2025-09-19 17:48:20 +0900

    fix: 프록시 설정 추가, cors 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,34 @@
-import { defineConfig } from 'vite';
+// vite.config.ts
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@app': path.resolve(__dirname, 'src/app'),
-      '@shared': path.resolve(__dirname, 'src/shared'),
-      '@entities': path.resolve(__dirname, 'src/entities'),
-      '@features': path.resolve(__dirname, 'src/features'),
-      '@widgets': path.resolve(__dirname, 'src/widgets'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
+export default defineConfig(({ mode }) => {
+  // mode = 'development' | 'production'
+  const env = loadEnv(mode, process.cwd(), ''); // '' prefix로 모든 env 로드
+  console.log('VITE_API_BASE_URL =', env.VITE_API_BASE_URL);
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL, // 여기서는 env 사용
+          changeOrigin: true,
+        },
+      },
     },
-  },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@app': path.resolve(__dirname, 'src/app'),
+        '@shared': path.resolve(__dirname, 'src/shared'),
+        '@entities': path.resolve(__dirname, 'src/entities'),
+        '@features': path.resolve(__dirname, 'src/features'),
+        '@widgets': path.resolve(__dirname, 'src/widgets'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+      },
+    },
+  };
 });
