c1eb1654493a1c113c25249328df73248b065d75
youngseo <givpro22@daum.net>
2025-09-26 01:12:04 +0900

    refactor: 추후 백엔드 명세서 작성완료시 낙관적 업데이트 작성을 위해 주석

--- src/features/like-post/model/useToggleLike.ts
+++ src/features/like-post/model/useToggleLike.ts
@@ -2,7 +2,10 @@ import { useMutation } from '@tanstack/react-query';
 import { likePost } from '../api/likePostApi';
 
 export function useToggleLike() {
+  // const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (postId: number) => likePost(postId),
+    // 추후 낙관적 업데이트 추가 예정
   });
 }
