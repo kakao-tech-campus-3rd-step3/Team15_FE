6a2b72317386c6184334b61e5112406341612218
youngseo <givpro22@daum.net>
2025-09-30 23:19:54 +0900

    feat: 게시글 상세정보 뒤로가기 버튼 라우트 연결

--- src/pages/post-detail/ui/PostDetailPage.tsx
+++ src/pages/post-detail/ui/PostDetailPage.tsx
@@ -3,16 +3,23 @@ import { SuspenseBoundary } from '@/shared/ui/suspense/SuspenseBoundary';
 import PostDetailContent from '@/widgets/PostDetailContent/ui/PostDetailContent';
 import PostDetailContentSkeleton from '@/widgets/PostDetailContent/ui/PostDetailContent.skeleton';
 
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export function PostDetailPage() {
   const { id } = useParams();
   const postId = Number(id);
-
+  const navigate = useNavigate();
   return (
     <>
       <section className='mx-8 space-y-6 pb-10 pt-10'>
-        <SectionHeader title='게시글 상세정보' left={<YSButton size='lg'>목록으로</YSButton>} />
+        <SectionHeader
+          title='게시글 상세정보'
+          left={
+            <YSButton size='lg' onClick={() => navigate(-1)}>
+              목록으로
+            </YSButton>
+          }
+        />
       </section>
       <main className='space-y-8 px-4 py-8'>
         <SuspenseBoundary fallback={<PostDetailContentSkeleton />}>
