02fcab59574da2ea9c9f066d973479617c22e64b
youngseo <givpro22@daum.net>
2025-10-04 16:04:17 +0900

    refactor: startsWith → matchPath로 변경해 부분 매칭 버그 수정

--- src/widgets/header/model/getActiveItem.ts
+++ src/widgets/header/model/getActiveItem.ts
@@ -1,5 +1,11 @@
 import { MENU_ITEMS } from '@/shared/config';
+import { matchPath } from 'react-router-dom';
 
 export function getActiveItem(pathname: string) {
-  return MENU_ITEMS.find((item) => pathname === item.url || pathname.startsWith(`${item.url}/`));
+  return MENU_ITEMS.find((item) => {
+    if (item.url === '/') {
+      return pathname === '/';
+    }
+    return !!matchPath({ path: item.url, end: false }, pathname);
+  });
 }
