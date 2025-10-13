2dccd871c1ec4786b0dc8a56446c630aafe2160a
youngseo <givpro22@daum.net>
2025-10-14 05:11:45 +0900

    fix: key 수정으로 import 오류 수정

--- src/entities/comment/model/useCommentQuery.ts
+++ src/entities/comment/model/useCommentQuery.ts
@@ -1,11 +1,11 @@
 import { commentKeys } from './queryKeys';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import type { CommentListResponse } from './comment.type';
-import { getComments } from '../api/comment.api';
+import { commentService } from '../lib/commentService';
 
 export function useComments(postId: number) {
   return useSuspenseQuery<CommentListResponse>({
     queryKey: commentKeys.listByPost(postId),
-    queryFn: () => getComments(postId),
+    queryFn: () => commentService.getComments(postId),
   });
 }
