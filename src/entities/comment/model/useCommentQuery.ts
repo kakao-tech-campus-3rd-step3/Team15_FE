5e2e1491527ffe1d81fcebcf3407170a5f754e2f
youngseo <givpro22@daum.net>
2025-10-14 04:46:40 +0900

    feat: 댓글 쿼리키 팩토리(commentKeys) 추가 및 useComments 적용

--- src/entities/comment/model/useCommentQuery.ts
+++ src/entities/comment/model/useCommentQuery.ts
@@ -1,10 +1,11 @@
+import { commentKeys } from './queryKeys';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { fetchComments } from '../api/comment.api';
 import type { CommentListResponse } from './comment.type';
 
 export function useComments(postId: number) {
   return useSuspenseQuery<CommentListResponse>({
-    queryKey: [postId],
+    queryKey: commentKeys.listByPost(postId),
     queryFn: () => fetchComments(postId),
   });
 }
