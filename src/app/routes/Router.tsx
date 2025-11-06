da89ec8453626e267d99f86c28b1672341e1e13a
jinhyun <jin71744@gmail.com>
2025-11-07 01:58:59 +0900

    feat: Kakao 로그인

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -16,14 +16,17 @@ import { SupportPage } from '@/pages/support';
 import { SupportDetailPage } from '@/pages/support-detail';
 import { ChatbotPage } from '@/pages/chatbot/ui/ChatbotPage';
 import ChatLayout from '../layout/ChatLayout';
-import OAuthCallback from '@/pages/oauth-callback/OAuthCallback';
+import GoogleOAuthCallback from '@/pages/oauth-callback/GoogleOAuthCallback';
+import { KakaoOAuthCallback } from '@/pages/oauth-callback/KakaoOAuthCallback';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AppLayout />}>
-          <Route path={ROUTES.oauthGoogle} element={<OAuthCallback />} />
+          <Route path={ROUTES.oauthGoogle} element={<GoogleOAuthCallback />} />
+          <Route path='/kakao/login/callback' element={<KakaoOAuthCallback />} />
+
           <Route path={ROUTES.landing} element={<LandingPage />} />
           <Route path={ROUTES.post} element={<HeartNewsPage />} />
           <Route path={ROUTES.login} element={<AuthPage />} />
