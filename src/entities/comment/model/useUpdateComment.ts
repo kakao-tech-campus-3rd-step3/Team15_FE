4254b00e8b7f551f14ee6542b29fbd54a45eb845
youngseo <givpro22@daum.net>
2025-11-13 10:16:08 +0900

    fix: 대댓글 수정 미반영 문제 수정

--- src/entities/comment/model/useUpdateComment.ts
+++ src/entities/comment/model/useUpdateComment.ts
@@ -14,13 +14,10 @@ export const useUpdateComment = () => {
   return useMutation({
     mutationFn: ({ commentId, body }: UpdateCommentVars) =>
       commentService.updateComment(commentId, body),
-    onSuccess: (_data, variables) => {
-      const { postId } = variables as UpdateCommentVars;
+    onSuccess: () => {
       // 개별 댓글 상세 및 목록 계열 무효화]
-      if (postId) {
-        qc.invalidateQueries({ queryKey: commentKeys.all });
-        qc.invalidateQueries({ queryKey: replyKeys.all });
-      }
+      qc.invalidateQueries({ queryKey: commentKeys.all });
+      qc.invalidateQueries({ queryKey: replyKeys.all });
     },
   });
 };
